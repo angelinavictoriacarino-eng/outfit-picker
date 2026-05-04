109fbd5e9a36586c7d63a6dd209c2d7211b55863
angelinavictoriacarino-eng <angelinavictoriacarino@gmail.com>
2026-05-04 13:46:21 -0500

    Add files via upload

---
+++ Outfit picker/Outfit picker/Outfit_pickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outfit_pickerApp.swift
+//  Outfit picker
+//
+//  Created by Carino, Angelina on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Outfit_pickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
